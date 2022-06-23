e716cfd94bbb2eeb0374e7edbdd892fdeaad432c
Oliver <1571880470@qq.com>
2022-06-23 17:14:45 +0800

    add codelldb but hard to use in nvim

--- lua/Oli/nvim-dap.lua
+++ lua/Oli/nvim-dap.lua
@@ -32,6 +32,31 @@ dap.listeners.before.event_exited["dapui_config"] = function()
   dapui.close()
 end
 
+-- -- debug for C, Cpp, Rust
+-- -- not completed
+-- dap.adapters.codelldb = {
+--   type = 'server',
+--   host = '127.0.0.1',
+--   port = 13000 -- 💀 Use the port printed out or specified with `--port`
+-- }
+--
+-- dap.configurations.cpp = {
+--   {
+--     name = "Launch file",
+--     type = "codelldb",
+--     request = "launch",
+--     program = function()
+--       return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+--     end,
+--     cwd = '${workspaceFolder}',
+--     stopOnEntry = true,
+--   },
+-- }
+-- dap.configurations.c = dap.configurations.cpp
+-- dap.configurations.rust = dap.configurations.cpp
+
+-- debug for Golang
 require('dap-go').setup()
+
 require("dapui").setup()
 
