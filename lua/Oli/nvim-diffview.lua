877f14f50436b4466a746bee8054764b0f71eb60
Oliver <1571880470@qq.com>
2022-07-13 16:34:14 +0800

    update plugs and solve conflicts in nvim-diffview

--- lua/Oli/nvim-diffview.lua
+++ lua/Oli/nvim-diffview.lua
@@ -25,14 +25,15 @@ require("diffview").setup({
     },
   },
   file_history_panel = {
-    log_options = {
-      max_count = 256,      -- Limit the number of commits
-      follow = false,       -- Follow renames (only for single file)
-      all = false,          -- Include all refs under 'refs/' including HEAD
-      merges = false,       -- List only merge commits
-      no_merges = false,    -- List no merge commits
-      reverse = false,      -- List commits in reverse order
-    },
+	-- NOTE: log_options在最新版本中已经弃用, 继续使用会有警告
+    -- log_options = {
+    --   max_count = 256,      -- Limit the number of commits
+    --   follow = false,       -- Follow renames (only for single file)
+    --   all = false,          -- Include all refs under 'refs/' including HEAD
+    --   merges = false,       -- List only merge commits
+    --   no_merges = false,    -- List no merge commits
+    --   reverse = false,      -- List commits in reverse order
+    -- },
     win_config = {          -- See ':h diffview-config-win_config'
       position = "bottom",
       height = 16,
