922c9a6743895d3d9a71d2f467938357d7ef3eb5
Oliver <1571880470@qq.com>
2022-11-15 16:36:46 +0800

    add -deprecated- for to-do-comments

--- lua/Oli/nvim-todo-comments.lua
+++ lua/Oli/nvim-todo-comments.lua
@@ -6,7 +6,7 @@ require("todo-comments").setup {
       FIX = {
         icon = " ", -- icon used for the sign, and in search results
         color = "error", -- can be a hex color, or a named color (see below)
-        alt = { "FIXME", "BUG", "FIXIT", "ISSUE", "ERROR" }, -- a set of other keywords that all map to this FIX keywords
+        alt = { "FIXME", "BUG", "FIXIT", "ISSUE", "ERROR" , "DEPRECATED"}, -- a set of other keywords that all map to this FIX keywords
         -- signs = false, -- configure signs for some keywords individually
       },
       TODO = { icon = " ", color = "info" },
