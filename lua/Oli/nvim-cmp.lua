11bf6e71eda51435e718c26e95b2cde620784bb9
Oliver <1571880470@qq.com>
2022-11-19 21:46:42 +0800

    change some trigger in luasnip

--- lua/Oli/nvim-cmp.lua
+++ lua/Oli/nvim-cmp.lua
@@ -127,43 +127,72 @@ cmp.setup({
             select = true,
             -- cmp.close()
         }),
-        ["<Tab>"] = function(fallback) -- see GH-231, GH-286
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif has_words_before() then
-            cmp.complete()
-          else
-            fallback()
-          end
-        end,
-        ["<S-Tab>"] = function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          else
-            fallback()
-          end
-        end,
-        -- ["<Tab>"] = cmp.mapping(function(fallback)
-        --     if cmp.visible() then
-        --         cmp.select_next_item()
-        --     elseif luasnip.expand_or_jumpable() then
+        -- ["<Tab>"] = function(fallback) -- see GH-231, GH-286
+        --   if cmp.visible() then
+        --     cmp.select_next_item()
+        --   elseif has_words_before() then
+        --     cmp.complete()
+        --   else
+        --     fallback()
+        --   end
+        -- end,
+        -- ["<S-Tab>"] = function(fallback)
+        --   if cmp.visible() then
+        --     cmp.select_prev_item()
+        --   else
+        --     fallback()
+        --   end
+        -- end,
+        ["zz"] = cmp.mapping(function(fallback)
+            if luasnip.expandable() then
+                luasnip.expand()
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+        ["<c-j>"] = cmp.mapping(function(fallback)
+            if luasnip.jumpable(1) then
+                luasnip.jump(1)
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+        -- -- can use <c-j> as the expand and jump trigger together
+        -- ["<c-j>"] = cmp.mapping(function(fallback)
+        --     if luasnip.expand_or_jumpable() then
         --         luasnip.expand_or_jump()
-        --     elseif has_words_before() then
-        --         cmp.complete()
         --     else
         --         fallback()
         --     end
         -- end, { "i", "s" }),
-        --
-        -- ["<S-Tab>"] = cmp.mapping(function(fallback)
-        --     if cmp.visible() then
-        --         cmp.select_prev_item()
-        --     elseif luasnip.jumpable(-1) then
-        --         luasnip.jump(-1)
-        --     else
-        --         fallback()
-        --     end
-        -- end, { "i", "s" }),
+        ["<c-k>"] = cmp.mapping(function(fallback)
+            if luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            elseif has_words_before() then
+                cmp.complete()
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
     },
     -- You can set mappings if you want
     -- mapping = insert_map,
@@ -225,17 +254,17 @@ cmp.setup({
 --     },
 -- })
 
- cmp.setup.cmdline('/', {
+cmp.setup.cmdline('/', {
     mapping = cmp.mapping.preset.cmdline(),
     sources = {
       { name = 'buffer' }
     }
-  })
- cmp.setup.cmdline(':', {
+})
+cmp.setup.cmdline(':', {
     mapping = cmp.mapping.preset.cmdline(),
     sources = cmp.config.sources({
       { name = 'path' }
     }, {
       { name = 'cmdline' }
     })
-  })
+})
