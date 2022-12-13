91fd405a21070156dadee9070313cf507cdcad09
Oliver <1571880470@qq.com>
2022-12-13 15:54:54 +0800

    alter mappings in nvim-telescope

--- nvim/lua/Oli/nvim-telescope.lua
+++ nvim/lua/Oli/nvim-telescope.lua
@@ -100,6 +100,8 @@ telescope.setup {
 
                 ["j"] = actions.move_selection_next,
                 ["k"] = actions.move_selection_previous,
+                ["<c-j>"] = actions.move_selection_next,
+                ["<c-k>"] = actions.move_selection_previous,
                 ["H"] = actions.move_to_top,
                 ["M"] = actions.move_to_middle,
                 ["L"] = actions.move_to_bottom,
