3060715f37a84236520bab1f10d547ee4836ce9c
Oliver <fei.chao.1009@gmail.com>
2022-12-25 11:38:37 +0800

    add Cltr-h to delete a pair

--- nvim/lua/Oli/nvim-autopairs.lua
+++ nvim/lua/Oli/nvim-autopairs.lua
@@ -34,7 +34,7 @@ npairs.setup({
     -- enable_bracket_in_quote = true, --
     -- map_cr = true,
     -- map_bs = false, -- map the <BS> key
-    -- map_c_h = false, -- Map the <C-h> key to delete a pair
+    map_c_h = true, -- Map the <C-h> key to delete a pair
     -- map_c_w = false, -- map <c-w> to delete a pair if possible
 })
 
