6d0b7b3da08917d9becae52fe94061268e47afe4
Oliver <fei.chao.1009@gmail.com>
2023-02-01 12:27:24 +0800

    Lazy v0.3.3

--- nvim/lua/plugins/lsp/init.lua
+++ nvim/lua/plugins/lsp/init.lua
@@ -1,12 +1,14 @@
 local lsp = require("plugins.lsp.lspconfig")
 local mason = require("plugins.lsp.mason")
 local fidget = require("plugins.lsp.fidget")
+local navic = require("plugins.lsp.nvim-navic")
 
 local others = require("plugins.lsp.others")
 return {
     lsp,
     mason,
     fidget,
+    navic,
 
     others,
 }
