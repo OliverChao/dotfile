d9b418141d3fb6797906f4a774852d68e6360bbd
Oliver <fei.chao.1009@gmail.com>
2023-02-01 16:49:48 +0800

    add rose-pine colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -97,5 +97,24 @@ return {
             }
         end
     },
+    {
+        'rose-pine/neovim',
+        name = 'rose-pine',
+        lazy = true,
+        -- priority = 1000,
+        config = function()
+            require('rose-pine').setup({
+                --- @usage 'main' | 'moon'
+                dark_variant = 'main',
+                bold_vert_split = false,
+                dim_nc_background = false,
+                disable_background = false,
+                disable_float_background = false,
+                disable_italics = true,
+            })
 
+            -- set colorscheme after options
+            -- vim.cmd('colorscheme rose-pine')
+        end
+    }
 }
