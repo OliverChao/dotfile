438ef6361342609abe7db3112614b9c4ef28f0fb
Oliver <1571880470@qq.com>
2023-02-14 09:24:06 +0800

    add notify ui file

--- nvim/lua/plugins/ui/init.lua
+++ nvim/lua/plugins/ui/init.lua
@@ -5,6 +5,7 @@ local dressing = require("plugins.ui.dressing")
 local bqf = require("plugins.ui.nvim-bqf")
 local indent_blank_line = require("plugins.ui.indent-blankline")
 local hlslens = require("plugins.ui.hlslens")
+local notify = require("plugins.ui.notify")
 
 return {
     dashboard,
@@ -14,21 +15,5 @@ return {
     bqf,
     indent_blank_line,
     hlslens,
-
-    {
-        -- A fancy, configurable, notification manager for NeoVim
-        'rcarriga/nvim-notify',
-        -- enabled = false,
-        config = function()
-            local notify = require("notify")
-            notify.setup({
-                render = "minimal", -- defult | minimal | simple | compact
-                stages = "static", -- fade_in_slide_out | fade | slide | static
-                timeout = 1000,
-            })
-            -- 使用 nvim-notify 插件代替 vim notify
-            vim.notify = notify
-        end
-    }
-
+    notify,
 }
