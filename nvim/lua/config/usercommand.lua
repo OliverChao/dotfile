585ce9b4351894c2aa4f937272dea82a12d94f70
Oliver <fei.chao.1009@gmail.com>
2023-03-02 20:22:03 +0800

    add Golint usercommand

--- nvim/lua/config/usercommand.lua
+++ nvim/lua/config/usercommand.lua
@@ -9,3 +9,25 @@ vim.api.nvim_create_user_command("WQ", "wq", { nargs = 0 })
 vim.api.nvim_create_user_command("Wq", "wq", { nargs = 0 })
 vim.api.nvim_create_user_command("Wa", "wa", { nargs = 0 })
 vim.api.nvim_create_user_command("Qa", "qa", { nargs = 0 })
+
+-- ╭──────────────────────────────────────────────────────────╮
+-- │                         Linters                          │
+-- ╰──────────────────────────────────────────────────────────╯
+
+vim.api.nvim_create_user_command("Golint", function()
+  if vim.bo.filetype == "go" then
+    if vim.fn.executable("golangci-lint") ~= 1 then
+      vim.notify("Missing golangci-lint dependency", vim.log.levels.WARN, {
+        on_open = function()
+          vim.notify(
+            "This linter[golangci-lint] can be download at\n[https://github.com/golangci/golangci-lint]",
+            "info",
+            { timeout = 3000 }
+          )
+        end,
+      })
+      return
+    end
+    vim.cmd([[ AsyncRun golangci-lint run % ]])
+  end
+end, { nargs = 0 })
