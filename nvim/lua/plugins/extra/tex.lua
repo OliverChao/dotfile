7762746aec115551a6fe1a3c6e06b05906922199
Oliver <1571880470@qq.com>
2023-03-20 08:57:06 +0800

    Hide warnings in vimtex

--- nvim/lua/plugins/extra/tex.lua
+++ nvim/lua/plugins/extra/tex.lua
@@ -6,6 +6,8 @@ return {
       -- vimtex
       vim.g.vimtex_view_method = "zathura"
       vim.g.vimtex_syntax_conceal_disable = 1
+      -- hide warnings
+      vim.g.vimtex_quickfix_open_on_warning = 0
     end,
   },
 }
