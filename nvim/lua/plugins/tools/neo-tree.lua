74ab5aeffe5d91d6845c480d617373382e247050
Oliver <1571880470@qq.com>
2023-04-19 08:39:59 +0800

    alter some neo-tree icons

--- nvim/lua/plugins/tools/neo-tree.lua
+++ nvim/lua/plugins/tools/neo-tree.lua
@@ -16,8 +16,8 @@ function M.config()
 
   vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
   vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
-  vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
-  vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHint" })
+  vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
+  vim.fn.sign_define("DiagnosticSignHint", { text = " ", texthl = "DiagnosticSignHint" })
 
   require("neo-tree").setup({
     popup_border_style = "rounded",
@@ -59,8 +59,8 @@ function M.config()
           -- Status type
           untracked = "",
           ignored = "◌",
-          unstaged = "✗",
-          staged = "",
+          unstaged = "",
+          staged = "",
           conflict = "",
         },
       },
