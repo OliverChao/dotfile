d19900d75a3e1c5c2c3ea2c685fa880852dc8a6f
Oliver <1571880470@qq.com>
2023-04-21 19:36:30 +0800

    add gotests.nvim

--- nvim/lua/plugins/extra/go.lua
+++ nvim/lua/plugins/extra/go.lua
@@ -6,4 +6,29 @@ return {
       require("gotests").setup()
     end,
   },
+  {
+    -- go implement tool
+    "edolphin-ydf/goimpl.nvim",
+    dir = "~/dotfile/nvim/lua/local/goimpl.nvim/",
+    dev = true,
+
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-lua/popup.nvim",
+      "nvim-telescope/telescope.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    ft = "go",
+    keys = {
+      {
+        "<leader>im",
+        [[<cmd>lua require'telescope'.extensions.goimpl.goimpl{}<CR>]],
+        { noremap = true, silent = true },
+        desc = "Golang impl",
+      },
+    },
+    config = function()
+      require("telescope").load_extension("goimpl")
+    end,
+  },
 }
