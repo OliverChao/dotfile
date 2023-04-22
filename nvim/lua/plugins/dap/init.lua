604e127f3d2895c53988143c28ec931b89b5d83e
Oliver <1571880470@qq.com>
2023-04-22 09:21:16 +0800

    refactor: nvim-dap && nvim-dap-go

--- nvim/lua/plugins/dap/init.lua
+++ nvim/lua/plugins/dap/init.lua
@@ -1,73 +1,27 @@
 local M = {
-  "mfussenegger/nvim-dap",
+  "rcarriga/nvim-dap-ui",
+  event = "VeryLazy",
   dependencies = {
-    { "leoluz/nvim-dap-go" },
-    { "rcarriga/nvim-dap-ui" },
+    "mfussenegger/nvim-dap",
   },
-  lazy = true,
 }
 
 function M.config()
-  --
-  -- using nvim-dap-go can remove vscode-go extension
+  -- nvim-dap settings.
   -- local dap = require("dap")
-  --
-  -- dap.adapters.go = {
-  --   type = 'executable';
-  --   command = 'node';
-  --   args = {'/home/oliver/GitRepositories/clone-git/vscode-go/dist/debugAdapter.js'};
-  -- }
-  -- dap.configurations.go = {
-  --   {
-  --     type = 'go';
-  --     name = 'Debug';
-  --     request = 'launch';
-  --     showLog = false;
-  --     program = "${file}";
-  --     dlvToolPath = vim.fn.exepath('dlv')  -- Adjust to where delve is installed
-  --   },
-  -- }
-  --
-  -- require("dapui").setup()
-  --
-  -- dapui auto-starts when dap starts
-  local dap, dapui = require("dap"), require("dapui")
-  dap.listeners.after.event_initialized["dapui_config"] = function()
-    dapui.open()
-  end
-  dap.listeners.before.event_terminated["dapui_config"] = function()
-    dapui.close()
-  end
-  dap.listeners.before.event_exited["dapui_config"] = function()
-    dapui.close()
-  end
+  vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "", linehl = "", numhl = "" })
 
-  -- -- debug for C, Cpp, Rust
-  -- -- not completed
-  -- dap.adapters.codelldb = {
-  --   type = 'server',
-  --   host = '127.0.0.1',
-  --   port = 13000 -- 💀 Use the port printed out or specified with `--port`
-  -- }
-  --
-  -- dap.configurations.cpp = {
-  --   {
-  --     name = "Launch file",
-  --     type = "codelldb",
-  --     request = "launch",
-  --     program = function()
-  --       return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-  --     end,
-  --     cwd = '${workspaceFolder}',
-  --     stopOnEntry = true,
-  --   },
-  -- }
-  -- dap.configurations.c = dap.configurations.cpp
-  -- dap.configurations.rust = dap.configurations.cpp
+  vim.keymap.set("n", "<leader>dk", function()
+    require("dap").continue()
+  end)
+  vim.keymap.set("n", "<leader>dl", function()
+    require("dap").run_last()
+  end)
+  vim.keymap.set("n", "<leader>db", function()
+    require("dap").toggle_breakpoint()
+  end)
 
-  -- debug for Golang
-  require("dap-go").setup()
-
+  -- nvim-dap-ui settings.
   require("dapui").setup()
 end
 
