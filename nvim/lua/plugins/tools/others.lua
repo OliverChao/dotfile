eb7dbecf7fcf573677d0a431252421ba7f562fb7
amotwang <amotwang@tencent.com>
2023-07-17 10:07:02 +0800

    move hop to moion section

--- nvim/lua/plugins/tools/others.lua
+++ nvim/lua/plugins/tools/others.lua
@@ -208,33 +208,6 @@ return {
     end,
   },
   {
-    "phaazon/hop.nvim",
-    lazy = "VeryLazy",
-    keys = {
-      -- all window search
-      {
-        "f",
-        "<cmd> lua require'hop'.hint_char1({ hint_position = require'hop.hint'.HintPosition.START })<cr>",
-        mode = { "n", "x" },
-      },
-
-      -- current line search
-      {
-        "t",
-        "<cmd>lua require'hop'.hint_char1({ current_line_only = true })<cr>",
-        mode = { "n", "x" },
-      },
-
-      -- go to lines
-      {
-        "<space>l",
-        "<cmd> lua require'hop'.hint_lines({ hint_position = require'hop.hint'.HintPosition.START })<cr>",
-        mode = { "n", "x" },
-      },
-    },
-    config = true,
-  },
-  {
     "kylechui/nvim-surround",
     event = "BufReadPost",
     opts = {
