fd10de4e1071e7d657a76a19b7857d0744998283
Oliver <1571880470@qq.com>
2023-10-06 21:45:17 +0800

    Update lazy-lock and update indent-line to V3

--- nvim/lua/plugins/ui/indent-blankline.lua
+++ nvim/lua/plugins/ui/indent-blankline.lua
@@ -1,69 +1,79 @@
 local M = {
   "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
   event = "BufReadPost",
 }
 
+-- version 3
 function M.config()
-  local ok, indent = pcall(require, "indent_blankline")
+  vim.cmd([[
+    highlight BoldFont guifg=#8A8A8A guisp=#d3d3d3
+  ]])
 
-  if not ok then
-    return
-  end
-
-  -- 自己不喜欢显示 空格 和 回车, 减少视觉压力
-  -- 并且不喜欢 显示 第一个 indentation
-  -- vim.opt.list = true
-  -- vim.opt.listchars:append "space:⋅"
-  -- vim.opt.listchars:append "eol:⤸"
-  -- vim.g.indent_blankline_context_char = '¦'
-  indent.setup({
-    char = "¦",
-    -- char = "┊",
-    -- char = "│",
-    context_char = "¦",
-    space_char_blankline = " ",
-    show_first_indent_level = false,
-    filetype_exclude = {
-      "startify",
-      "dashboard",
-      "dotooagenda",
-      "log",
-      "fugitive",
-      "gitcommit",
-      "packer",
-      "vimwiki",
-      -- "markdown",
-      "json",
-      "txt",
-      "vista",
-      "help",
-      "todoist",
-      "NvimTree",
-      "peekaboo",
-      "git",
-      "TelescopePrompt",
-      "undotree",
-      "flutterToolsOutline",
-      "", -- for all buffers without a file type
+  require("ibl").setup({
+    indent = {
+      char = "¦",
     },
-    buftype_exclude = { "terminal", "nofile" },
-    show_trailing_blankline_indent = true,
-    show_current_context = true,
-    show_current_context_start = false,
-    context_patterns = {
-      "class",
-      "function",
-      "method",
-      "block",
-      "list_literal",
-      "selector",
-      "^if",
-      "^table",
-      "if_statement",
-      "while",
-      "for",
+    scope = {
+      enabled = true,
+      char = "¦",
+      show_start = false,
+      show_end = false,
+      highlight = {
+        "BoldFont",
+      },
     },
   })
+
+  -- version 2 configuration
+  -- require("ibl").setup({
+  --   char = "¦",
+  --   -- char = "┊",
+  --   -- char = "│",
+  --   context_char = "¦",
+  --   space_char_blankline = " ",
+  --   show_first_indent_level = false,
+  --   filetype_exclude = {
+  --     "startify",
+  --     "dashboard",
+  --     "dotooagenda",
+  --     "log",
+  --     "fugitive",
+  --     "gitcommit",
+  --     "packer",
+  --     "vimwiki",
+  --     -- "markdown",
+  --     "json",
+  --     "txt",
+  --     "vista",
+  --     "help",
+  --     "todoist",
+  --     "NvimTree",
+  --     "peekaboo",
+  --     "git",
+  --     "TelescopePrompt",
+  --     "undotree",
+  --     "flutterToolsOutline",
+  --     "", -- for all buffers without a file type
+  --   },
+  --   buftype_exclude = { "terminal", "nofile" },
+  --   show_trailing_blankline_indent = true,
+  --   show_current_context = true,
+  --   show_current_context_start = false,
+  --   context_patterns = {
+  --     "class",
+  --     "function",
+  --     "method",
+  --     "block",
+  --     "list_literal",
+  --     "selector",
+  --     "^if",
+  --     "^table",
+  --     "if_statement",
+  --     "while",
+  --     "for",
+  --   },
+  -- })
 end
 
 return M
