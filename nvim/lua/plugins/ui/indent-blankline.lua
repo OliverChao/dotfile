07394bf91ede6d9018d9833e86c4f2972f809d37
Oliver <1571880470@qq.com>
2023-10-07 10:59:49 +0800

    make indent-blankline v3 similar to v2

--- nvim/lua/plugins/ui/indent-blankline.lua
+++ nvim/lua/plugins/ui/indent-blankline.lua
@@ -10,13 +10,17 @@ function M.config()
     highlight BoldFont guifg=#8A8A8A guisp=#d3d3d3
   ]])
 
+  local hooks = require("ibl.hooks")
+  hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
+  hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_tab_indent_level)
+  -- hooks.register(hooks.type.SKIP_LINE, hooks.builtin.skip_preproc_lines)
+
   require("ibl").setup({
     indent = {
       char = "¦",
     },
     scope = {
       enabled = true,
-      char = "¦",
       show_start = false,
       show_end = false,
       highlight = {
