2f9d8cb2663f1ec68c9deff8b7839e93aa7f0d22
Oliver <1571880470@qq.com>
2023-12-02 22:36:47 +0800

    make git-conflict work only with cmd

--- nvim/lua/plugins/git/git-conflict.lua
+++ nvim/lua/plugins/git/git-conflict.lua
@@ -1,27 +1,39 @@
 local M = {
   "akinsho/git-conflict.nvim",
-  event = "BufReadPre",
-  keys = {
-    { "[x", "<Plug>(git-conflict-prev-conflict)" },
-    { "]x", "<Plug>(git-conflict-next-conflict)" },
-    { "<leader>c1", "<Plug>(git-conflict-ours)", desc = "[git-conflict] select ours" },
-    { "<leader>c2", "<Plug>(git-conflict-theirs)", desc = "[git-conflict] select theirs" },
-    { "<leader>c3", "<Plug>(git-conflict-both)", desc = "[git-conflict] select both" },
-    { "<leader>c0", "<Plug>(git-conflict-none)", desc = "[git-conflict] select none" },
+  -- enabled = false,
+  -- event = "BufReadPre",
+  cmd = {
+    "GitConflictRefresh",
   },
-}
-
-function M.config()
-  require("git-conflict").setup({
+  -- keys = {
+  --   { "[x", "<Plug>(git-conflict-prev-conflict)" },
+  --   { "]x", "<Plug>(git-conflict-next-conflict)" },
+  --   { "<leader>c1", "<Plug>(git-conflict-ours)", desc = "[git-conflict] select ours" },
+  --   { "<leader>c2", "<Plug>(git-conflict-theirs)", desc = "[git-conflict] select theirs" },
+  --   { "<leader>c3", "<Plug>(git-conflict-both)", desc = "[git-conflict] select both" },
+  --   { "<leader>c0", "<Plug>(git-conflict-none)", desc = "[git-conflict] select none" },
+  -- },
+  opts = {
     default_mappings = false, -- disable buffer local mapping created by this plugin
     default_commands = true, -- disable commands created by this plugin
     disable_diagnostics = false, -- This will disable the diagnostics in a buffer whilst it is conflicted
+    list_opener = "copen", -- command or function to open the conflicts list
     highlights = { -- They must have background color, otherwise the default color will be used
-      -- incoming = 'DiffText',
       incoming = "DiffAdd",
       current = "DiffAdd",
+      -- current = "DiffText",
     },
-  })
+  },
+}
+
+function M.config(_, opts)
+  require("git-conflict").setup(opts)
+  vim.keymap.set("n", "<leader>c1", "<Plug>(git-conflict-ours)")
+  vim.keymap.set("n", "<leader>c2", "<Plug>(git-conflict-theirs)")
+  vim.keymap.set("n", "<leader>c3", "<Plug>(git-conflict-both)")
+  vim.keymap.set("n", "<leader>c0", "<Plug>(git-conflict-none)")
+  vim.keymap.set("n", "[x", "<Plug>(git-conflict-prev-conflict)")
+  vim.keymap.set("n", "]x", "<Plug>(git-conflict-next-conflict)")
 end
 
 return M
